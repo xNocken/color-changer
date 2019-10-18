3313cd022d30c3323add0a746939dae057152129
Marc Lübke <marc.luebke@gal-digital.de>
2019-10-18 18:55:39 +0200

    refactor(js): seperated files

--- src/js/request.js
+++ src/js/request.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 
 import routes from './routes';
 import message from './message';
-import { createElement } from './slider';
+import { setTheme, createElement } from './renderer';
 
 const getThemes = () => {
   $.get(routes.get).done((response) => {
@@ -18,18 +18,84 @@ const getThemes = () => {
   });
 };
 
-const saveTheme = () => {
+const saveTheme = (theme) => {
+  $.get(routes.save, { theme }).done((response) => {
+    if (response) {
+      const { id, msg } = JSON.parse(response);
+
+      message(msg);
+      createElement({ id, ...theme });
+    }
+  });
 };
 
-const updateTheme = () => {
+const updateTheme = (id) => {
+  const theme = $(`[data-id="${id}"]`).data('theme');
+  $(`[data-id="${id}"]`).addClass('edit');
+
+  setTheme(id);
+  $('#text').val(theme.name);
 };
 
-const deleteTheme = () => {
+const deleteTheme = (id) => {
+  $.get(routes.delete, { id }).done((response) => {
+    message(response);
+  });
+
+  $(`[data-id="${id}"`).remove();
+  $(`[data-delete-id="${id}"`).remove();
+  $(`[data-edit-id="${id}"`).remove();
 };
+
+const saveEditedTheme = (rgb, name, id) => {
+  const theme = $(`[data-id="${id}"]`).data('theme');
+  const { r, g, b } = rgb;
+  const newId = theme.id;
+
+  const newElem = $(`[data-id="${newId}"`);
+  message(newId);
+
+  newElem.data('theme', {
+    name,
+    r,
+    g,
+    b,
+    id: newId,
+  });
+
+  newElem.text(name);
+
+  $.get(routes.update, { theme }).done((response) => {
+    if (response) {
+      message(response);
+    }
+  });
+
+  $('.edit').removeClass('edit');
+};
+
+const handleSave = (rgb, name) => {
+  const { r, g, b } = rgb;
+  const editElem = $('.edit');
+
+  if (editElem.length !== 0) {
+    saveEditedTheme(rgb, name, editElem.data('theme').id);
+  } else if (name) {
+    saveTheme({
+      r,
+      g,
+      b,
+      name,
+    });
+  } else {
+    message('Name darf nicht leer sein');
+  }
+};
+
 
 export default {
   delete: () => deleteTheme(),
   get: () => getThemes(),
-  save: () => saveTheme(),
+  save: () => handleSave(),
   update: () => updateTheme(),
 };
